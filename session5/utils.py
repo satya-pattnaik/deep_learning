ebd4fbb122bf38e9e1d64ef3d0609c8e4761a197
Satya Prakash Pattnaik <satyapattnaik76@gmail.com>
2023-06-03 01:56:55 +0530

    Update utils.py

--- session5/utils.py
+++ session5/utils.py
@@ -1,1 +1,102 @@
+import torch
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from tqdm import tqdm
 
+# Data to plot accuracy and loss graphs
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+def get_losses():
+    return train_losses,test_losses,train_acc,test_acc
+
+def training_data_transformer():
+    return transforms.Compose([
+              transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+              transforms.Resize((28, 28)),
+              transforms.RandomRotation((-15., 15.), fill=0),
+              transforms.ToTensor(),
+              transforms.Normalize((0.1307,), (0.3081,)),
+            ])
+
+def test_data_transformer():
+    return transforms.Compose([
+              transforms.ToTensor(),
+              transforms.Normalize((0.1307,), (0.3081,))
+            ])
+
+def plot_images_in_batch(batch_data,batch_label,num_of_imgs=12):
+    fig = plt.figure()
+
+    for i in range(num_of_imgs):
+      plt.subplot(3,4,i+1)
+      plt.tight_layout()
+      plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+      plt.title(batch_label[i].item())
+      plt.xticks([])
+      plt.yticks([])
+
+
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = F.nll_loss(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def test(model, device, test_loader):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
